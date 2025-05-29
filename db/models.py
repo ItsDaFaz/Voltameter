ab7239a7d0f0fcdf1f2bb3ddb8c7295ad594d78f
Faiyaz Murshed <murshed.faiyaz@gmail.com>
2025-05-30 02:55:45 +0600

    added models for members and messages

---
+++ db/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
+
+class Member(Base):
+    __tablename__ = 'members'
+    id = Column(Integer, primary_key=True, index=True)
+    # Add more attributes later
+    messages = relationship('Message', back_populates='author')
+
+class Message(Base):
+    __tablename__ = 'messages'
+    id = Column(Integer, primary_key=True, index=True)
+    author_id = Column(Integer, ForeignKey('members.id'))
+    timestamp = Column(DateTime, nullable=False)
+    in_voice = Column(Boolean, default=False)
+
+    author = relationship('Member', back_populates='messages')
