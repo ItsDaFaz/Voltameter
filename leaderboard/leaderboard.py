633156dfd181441f0a1a9844d7c5cc44accfc86a
Faiyaz Murshed <murshed.faiyaz@gmail.com>
2025-06-09 03:54:52 +0600

    added bonus points to embed

--- leaderboard/leaderboard.py
+++ leaderboard/leaderboard.py
@@ -6,7 +6,7 @@ from typing import Optional, List
 import discord
 from discord.ext import tasks
 from discord import Guild, TextChannel, ForumChannel, Member, Embed, Color, Thread, Role, VoiceChannel
-from config import  DESTINATION_CHANNEL_ID_DEV as DESTINATION_CHANNEL_ID, GUILD_ID, MR_ELECTRICITY_ROLE_ID, HIGH_VOLTAGE_ROLE_ID, ADMIN_ROLES_IDS, TEXT_CHANNEL_LIST, FORUM_CHANNEL_LIST, EMBED_DESCRIPTION, EMBED_TITLE, EMBED_COLOR
+from config import  DESTINATION_CHANNEL_ID as DESTINATION_CHANNEL_ID, GUILD_ID, MR_ELECTRICITY_ROLE_ID, HIGH_VOLTAGE_ROLE_ID, ADMIN_ROLES_IDS, TEXT_CHANNEL_LIST, FORUM_CHANNEL_LIST, EMBED_DESCRIPTION, EMBED_TITLE, EMBED_COLOR
 from utils.helpers import escape_markdown
 
 from db.session import get_engine, get_session_maker
@@ -31,8 +31,8 @@ class LeaderboardManager:
         self.channel_message_counts = {}
         self.forum_message_counts = {}
 
-        # self.is_prod = IS_PROD
-        self.is_prod = True  # For testing purposes
+        self.is_prod = IS_PROD
+        # self.is_prod = True  # For testing purposes
         # Create engine and sessionmaker for this thread/event loop
         self.engine = get_engine()
         self.SessionLocal = get_session_maker(self.engine)
@@ -147,6 +147,9 @@ class LeaderboardManager:
                     and not ({role.id for role in member.roles} & set(ADMIN_ROLES_IDS))
                     and not any(role.permissions.administrator for role in member.roles)
                 )
+                # if (
+                #     isinstance(member, Member)
+                # )
             }
             top_ten = Counter(non_admin_messages).most_common(10)
 
@@ -188,7 +191,10 @@ class LeaderboardManager:
                 total_points = channel_message_points + db_message_points
 
                 memberName = escape_markdown(member.display_name)
-                embed_content += f"`{idx+1}` **{memberName}** — `{total_points}` volt\n"
+                embed_content += f"`{idx+1}` **{memberName}** — `{total_points}` volt"
+                if db_message_points != 0:
+                    embed_content += f"\t<:_:1380603159906619452>`+{db_message_points}`"
+                embed_content += "\n"
                 top_ten_list.append(member.id)
         embed_content += f"\nBased on last `{str(await self.get_leaderboard_days())}` **days** of messaging activities."
         if not embed_content:
@@ -237,6 +243,8 @@ class LeaderboardManager:
                 print(f"Error cleaning previous messages: {e}")
             self.cached_leaderboard_embed = embed
             await destination_channel.send(embed=embed)
+            
+            #Role assignment logic
             mr_electricity_role: Optional[Role] = discord.utils.get(guild.roles, id=MR_ELECTRICITY_ROLE_ID)
             high_voltage_role: Optional[Role] = discord.utils.get(guild.roles, id=HIGH_VOLTAGE_ROLE_ID)
             if not high_voltage_role or not mr_electricity_role:
@@ -284,6 +292,7 @@ class LeaderboardManager:
                         print(f"Error adding Mr. Electricity to {current_top_member.name}: {e}", flush=True)
             except Exception as e:
                 print(f"Unexpected error during role management: {e}", flush=True)
+            # Role management logic end
         except Exception as e:
             print(f"Error in auto leaderboard task: {e}, will retry in 5 minutes.",flush=True)
 
