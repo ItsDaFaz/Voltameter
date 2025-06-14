261aa019c4164a06609121b705278cd798d972e1
Faiyaz Murshed <murshed.faiyaz@gmail.com>
2025-06-14 14:44:41 +0600

    In members, guild_id is now an array; removed foreign key relationship. Added multiplers to guild

--- db/models.py
+++ db/models.py
@@ -13,6 +13,8 @@ class Guild(Base):
     forum_channels_list = Column(ARRAY(BigInteger))
     destination_channel_id = Column(BigInteger, nullable=True)
     destination_channel_id_dev = Column(BigInteger, nullable=True)
+    text_multiplier = Column(Integer, nullable=True)  # New field for text multiplier
+    in_voice_boost_multiplier = Column(Integer, nullable=True)  # New field for in-voice boost multiplier
     members = relationship('Member', back_populates='guild')
     messages = relationship('Message', back_populates='guild')
     
@@ -20,9 +22,11 @@ class Guild(Base):
 class Member(Base):
     __tablename__ = 'members'
     id = Column(BigInteger, primary_key=True, index=True)
-    guild_id = Column(BigInteger, ForeignKey('guilds.id'), nullable=False)
+    guild_id = Column(ARRAY(BigInteger), nullable=False)  # Changed from BigInteger to ARRAY(BigInteger)
     messages = relationship('Message', back_populates='author')
-    guild = relationship('Guild', back_populates='members')
+    # Removed ForeignKey constraint from guild_id, as it is now an array
+    # Relationship to Guild may need to be rethought, as a member can now belong to multiple guilds
+    # guild = relationship('Guild', back_populates='members')
 
 class Message(Base):
     __tablename__ = 'messages'
